76756831f50699c9541085f5c44935259c1d41ac
Md Taufique Hussain <taufique71@gmail.com>
2018-05-26 14:07:10 +0600

    `POST /bid` accepts nested JSON in request body

--- src/main/scala/com/bidding/Main.scala
+++ src/main/scala/com/bidding/Main.scala
@@ -8,13 +8,30 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json._
 
-case class Site(
-  id: Int, 
-  domain: String
+case class Site( id: Int, domain: String )
+case class Geo(
+  country: Option[String], 
+  city: Option[String], 
+  lat: Option[Double], 
+  lon: Option[Double]
 )
+case class Impression(
+  id: String, 
+  wmin: Option[Int],
+  wmax: Option[Int],
+  w: Option[Int],
+  hmin: Option[Int],
+  hmax: Option[Int],
+  h: Option[Int],
+  bidFloor: Option[Double]
+)
+case class User(id: String, geo: Option[Geo])
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val siteFormat = jsonFormat2(Site)
+  implicit val geoFormat = jsonFormat4(Geo)
+  implicit val impressionFormat = jsonFormat8(Impression)
+  implicit val userFormat = jsonFormat2(User)
 }
 
 object Server extends App with JsonSupport {
@@ -30,8 +47,12 @@ object Server extends App with JsonSupport {
     } ~
     path("bid") { 
       post {
-        entity(as[Site]) { site =>
-          println(site.id, site.domain)
+        entity(as[User]) { user =>
+          println(user)
+          user.geo match {
+            case Some(g) => println(g)
+            case None => println("User has no geolocation")
+          }
           complete("Hello Bidder!") 
         }
       }
