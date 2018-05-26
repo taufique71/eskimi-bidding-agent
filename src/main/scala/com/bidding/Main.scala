df786a131de491e2196dc5b1fcdaf8ee5fcd7d95
Md Taufique Hussain <taufique71@gmail.com>
2018-05-26 12:48:23 +0600

    `POST /bid` route accepts simple JSON format on request body

--- src/main/scala/com/bidding/Main.scala
+++ src/main/scala/com/bidding/Main.scala
@@ -5,13 +5,19 @@ import scala.concurrent.Future
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json._
 
 case class Site(
   id: Int, 
   domain: String
 )
 
-object Server extends App {
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val siteFormat = jsonFormat2(Site)
+}
+
+object Server extends App with JsonSupport {
   implicit val system = ActorSystem("actor-system")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val ec = system.dispatcher
@@ -24,7 +30,10 @@ object Server extends App {
     } ~
     path("bid") { 
       post {
-        complete("Hello Bidder!") 
+        entity(as[Site]) { site =>
+          println(site.id, site.domain)
+          complete("Hello Bidder!") 
+        }
       }
     }
   val bindingFuture = Http().bindAndHandle(routes, "localhost", 8080)
