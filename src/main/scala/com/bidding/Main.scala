42f1608e418551abc0e0e25c62479c4cdf5815a3
Md Taufique Hussain <taufique71@gmail.com>
2018-05-26 14:25:52 +0600

    `POST /bid` now accepts required format of bid request

--- src/main/scala/com/bidding/Main.scala
+++ src/main/scala/com/bidding/Main.scala
@@ -9,29 +9,21 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json._
 
 case class Site( id: Int, domain: String )
-case class Geo(
-  country: Option[String], 
-  city: Option[String], 
-  lat: Option[Double], 
-  lon: Option[Double]
-)
-case class Impression(
-  id: String, 
-  wmin: Option[Int],
-  wmax: Option[Int],
-  w: Option[Int],
-  hmin: Option[Int],
-  hmax: Option[Int],
-  h: Option[Int],
-  bidFloor: Option[Double]
+case class Geo( country: Option[String], city: Option[String], lat: Option[Double], lon: Option[Double] )
+case class Impression( id: String, wmin: Option[Int], wmax: Option[Int], w: Option[Int],
+  hmin: Option[Int], hmax: Option[Int], h: Option[Int], bidFloor: Option[Double]
 )
 case class User(id: String, geo: Option[Geo])
+case class Device(id: String, geo: Option[Geo])
+case class BidRequest(id: String, impression: Option[List[Impression]], site: Site, user: Option[User], device: Option[Device])
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val siteFormat = jsonFormat2(Site)
   implicit val geoFormat = jsonFormat4(Geo)
   implicit val impressionFormat = jsonFormat8(Impression)
   implicit val userFormat = jsonFormat2(User)
+  implicit val deviceFormat = jsonFormat2(Device)
+  implicit val bidRequestFormat = jsonFormat5(BidRequest)
 }
 
 object Server extends App with JsonSupport {
@@ -47,12 +39,12 @@ object Server extends App with JsonSupport {
     } ~
     path("bid") { 
       post {
-        entity(as[User]) { user =>
-          println(user)
-          user.geo match {
-            case Some(g) => println(g)
-            case None => println("User has no geolocation")
-          }
+        entity(as[BidRequest]) { bidRequest =>
+          println(bidRequest)
+          //user.geo match {
+            //case Some(g) => println(g)
+            //case None => println("User has no geolocation")
+          //}
           complete("Hello Bidder!") 
         }
       }
