983f8be3ad95679dadf834286acd3798cc2eaee6
Md Taufique Hussain <taufique71@gmail.com>
2018-05-27 11:58:59 +0600

    `POST /bid` endpoint logic completed partially

--- src/main/scala/com/bidding/Main.scala
+++ src/main/scala/com/bidding/Main.scala
@@ -4,9 +4,12 @@ import akka.actor.ActorSystem
 import scala.concurrent.Future
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json._
+import scala.util.control._
 
 case class Site( id: Int, domain: String )
 case class Geo( country: Option[String], city: Option[String], lat: Option[Double], lon: Option[Double] )
@@ -16,14 +19,15 @@ case class Impression( id: String, bidFloor: Option[Double],
 )
 case class User( id: String, geo: Option[Geo] )
 case class Device( id: String, geo: Option[Geo] )
-case class BidRequest( id: String, impression: Option[List[Impression]], site: Site, user: Option[User], device: Option[Device] )
+case class BidRequest( id: String, imp: Option[List[Impression]], site: Site, 
+  user: Option[User], device: Option[Device] )
 case class TimeRange( timeStart: Long, timeEnd: Long )
-case class Targeting( cities: List[String], targetedSiteIds: List[Int] )
+case class Targeting( cities: List[String], targetedSiteIds: Set[Int] )
 case class Banner( id: Int, src: String, width: Int, height: Int)
-case class Campaign( id: Int, userId: Int, country: String, bid: Double,
-  runningTimes: Set[TimeRange], targeting: Targeting, banners: List[Banner]
+case class Campaign( id: Int, userId: Int, country: String,
+  runningTimes: Set[TimeRange], targeting: Targeting, banners: List[Banner], bid: Double
 )
-case class BidResponse( id: String, bidRequestId: String, price: Double, adid: Option[String], banner: Option[Banner])
+case class BidResponse( id: Int, bidRequestId: String, price: Double, adid: Option[String], banner: Option[Banner])
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val siteFormat = jsonFormat2(Site)
@@ -31,7 +35,7 @@ trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val impressionFormat = jsonFormat8(Impression)
   implicit val userFormat = jsonFormat2(User)
   implicit val deviceFormat = jsonFormat2(Device)
-  implicit val bidRequestFormat = jsonFormat5(BidRequest)
+  implicit def bidRequestFormat = jsonFormat5(BidRequest)
   implicit val timeRangeFormat = jsonFormat2(TimeRange)
   implicit val targetingFormat = jsonFormat2(Targeting)
   implicit val bannerFormat = jsonFormat4(Banner)
@@ -43,10 +47,37 @@ object Server extends App with JsonSupport {
   implicit val system = ActorSystem("actor-system")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val ec = system.dispatcher
+  val r = scala.util.Random
 
-  //val campaigns: List[Campaign] = List(
-    //Campaign()  
-  //)
+  val campaignPool: List[Campaign] = List(
+    Campaign( 
+      123, 
+      213, 
+      "Bangladesh", 
+      Set(TimeRange(1600, 1700), TimeRange(2030, 2230)), 
+      Targeting(List("Dhaka", "Chuadanga"),Set(122429, 104797)), 
+      List(Banner(321, "BDFlag", 10, 6), Banner(32, "Random Bangladeshi Banner", 15, 14)),
+      10.34
+    ), 
+    Campaign( 
+      124, 
+      214, 
+      "India", 
+      Set(TimeRange(1300, 1415), TimeRange(1840, 2000)), 
+      Targeting(List("Mumbai", "Bengalore"),Set(31, 104798)), 
+      List(Banner(321, "IndiaFlag", 10, 6), Banner(32, "Random Indian Banner", 15, 14)),
+      1.6
+    ), 
+    Campaign( 
+      125, 
+      215, 
+      "Some Country", 
+      Set(TimeRange(1300, 1415), TimeRange(1840, 2000)), 
+      Targeting(List("City A", "City B"),Set(122430, 104798)), 
+      List(Banner(321, "SomeFlag", 10, 6), Banner(32, "Some Random Banner", 15, 14)),
+      1.6
+    ) 
+  )
 
   val routes = 
     path("") { 
@@ -57,12 +88,28 @@ object Server extends App with JsonSupport {
     path("bid") { 
       post {
         entity(as[BidRequest]) { bidRequest =>
-          println(bidRequest)
-          //user.geo match {
-            //case Some(g) => println(g)
-            //case None => println("User has no geolocation")
-          //}
-          complete("Hello Bidder!") 
+          var bidResponse: Option[BidResponse] = None
+          for(campaign <- campaignPool){
+            if(campaign.targeting.targetedSiteIds(bidRequest.site.id)){
+              val impressions = bidRequest.imp match {
+                case Some(x) => x
+                case None => None
+              }
+              if(impressions != None){
+                //println(impressions)
+                //for(i <- impressions){
+                  //println(i)
+                //}
+              }
+              else{
+                bidResponse = Some(BidResponse(r.nextInt(1000), bidRequest.id, campaign.bid, None, None))
+              }
+            }
+          }
+          bidResponse match {
+            case Some(r) => complete(r)
+            case None => complete(StatusCodes.NoContent)
+          }
         }
       }
     }
